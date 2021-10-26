95ed854469c37ed4e0b6083586059d793764ecfe
riku0202 <riku0202excalibur@gmail.com>
2021-10-26 22:41:25 +0900

    更新

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useReducer } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { Facebook, Instagram } from "./SVG";
 
 export const Layout = ({ children }: { children: JSX.Element }) => {
+  const [isOpen, switchIsOpen] = useReducer((state) => !state, false);
   return (
     <>
       <GlobalStyle />
@@ -18,8 +19,9 @@ export const Layout = ({ children }: { children: JSX.Element }) => {
       </Head>
       <main>
         <Style>
-          <div className="layout-background">
-            <div className="layout-menu">
+          {/* pc用 */}
+          <div className="pc-background">
+            <div className="pc-menu">
               <a href="#top">top</a>
               <a href="#about">about</a>
               <a href="#news">news</a>
@@ -35,6 +37,16 @@ export const Layout = ({ children }: { children: JSX.Element }) => {
               <a href="https://www.facebook.com/Number-A-Coffee-1580096218922774/">
                 {Facebook()}
               </a>
+            </div>
+          </div>
+          {/* スマホ・タブレット用 */}
+          <div className="sp-menu">
+            <div
+              className={isOpen ? "menu-trigger active" : "menu-trigger"}
+              onClick={switchIsOpen}>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
           </div>
           <div className="layout-image">
@@ -70,7 +82,7 @@ const Style = styled.div`
   width: 100%;
   height: 100vh;
 
-  .layout-background {
+  .pc-background {
     position: fixed;
     top: 0;
     right: 0;
@@ -79,7 +91,7 @@ const Style = styled.div`
     background: linear-gradient(to left, #ffffff60 70%, #ffffff01);
     z-index: 9;
 
-    .layout-menu {
+    .pc-menu {
       position: absolute;
       width: 60%;
       top: 50%;
@@ -138,6 +150,157 @@ const Style = styled.div`
       transform: translateX(-50%) translateY(-50%);
       min-width: 100%;
       min-height: 100%;
+    }
+  }
+
+  // スマホ・タブレット
+  @media (max-width: 959px) {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+
+    .pc-background {
+      display: none;
+    }
+
+    // btnのスタイル
+    .sp-menu {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 30;
+
+      .menu-trigger,
+      .menu-trigger span {
+        display: inline-block;
+        transition: all 0.4s;
+        box-sizing: border-box;
+      }
+      .menu-trigger {
+        position: relative;
+        width: 50px;
+        height: 44px;
+        background: none;
+        border: none;
+        appearance: none;
+        cursor: pointer;
+      }
+      .menu-trigger span {
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background-color: #000000;
+        border-radius: 4px;
+      }
+      .menu-trigger span:nth-of-type(1) {
+        top: 0;
+      }
+      .menu-trigger span:nth-of-type(2) {
+        top: 20px;
+      }
+      .menu-trigger span:nth-of-type(3) {
+        bottom: 0;
+      }
+
+      .menu-trigger span:nth-of-type(1) {
+        animation: menu-bar01 0.75s forwards;
+      }
+      @keyframes menu-bar01 {
+        0% {
+          transform: translateY(20px) rotate(45deg);
+        }
+        50% {
+          transform: translateY(20px) rotate(0);
+        }
+        100% {
+          transform: translateY(0) rotate(0);
+        }
+      }
+      .menu-trigger span:nth-of-type(2) {
+        transition: all 0.25s 0.25s;
+        opacity: 1;
+      }
+      .menu-trigger span:nth-of-type(3) {
+        animation: menu-bar02 0.75s forwards;
+      }
+      @keyframes menu-bar02 {
+        0% {
+          transform: translateY(-20px) rotate(-45deg);
+        }
+        50% {
+          transform: translateY(-20px) rotate(0);
+        }
+        100% {
+          transform: translateY(0) rotate(0);
+        }
+      }
+      .menu-trigger.active span:nth-of-type(1) {
+        animation: active-menu-bar01 0.75s forwards;
+      }
+      @keyframes active-menu-bar01 {
+        0% {
+          transform: translateY(0) rotate(0);
+        }
+        50% {
+          transform: translateY(20px) rotate(0);
+        }
+        100% {
+          transform: translateY(20px) rotate(45deg);
+        }
+      }
+      .menu-trigger.active span:nth-of-type(2) {
+        opacity: 0;
+      }
+      .menu-trigger.active span:nth-of-type(3) {
+        animation: active-menu-bar03 0.75s forwards;
+      }
+      @keyframes active-menu-bar03 {
+        0% {
+          transform: translateY(0) rotate(0);
+        }
+        50% {
+          transform: translateY(-20px) rotate(0);
+        }
+        100% {
+          transform: translateY(-20px) rotate(-45deg);
+        }
+      }
+    }
+
+    .layout-image {
+      position: fixed;
+      height: 100px;
+      width: 100px;
+      z-index: 10;
+      top: -10px;
+      left: -10px;
+    }
+
+    .video-wrap {
+      position: fixed;
+      height: 100vh;
+      width: 100vw;
+      overflow: hidden;
+
+      .number-a-coffee {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 80px;
+        font-weight: bold;
+      }
+
+      .video {
+        position: absolute;
+        z-index: -10;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        min-width: 100%;
+        min-height: 100%;
+      }
     }
   }
 `;
