4c50a7ab8a45eb70f8b5309a3da51cd868a9d86c
riku0202 <riku0202excalibur@gmail.com>
2022-07-26 22:59:33 +0900

    css変数を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect } from "react";
 import "../../public/reset.css";
 
 type NextPageWithLayout = NextPage & {
@@ -13,6 +13,12 @@ type AppPropsWithLayout = AppProps & {
 
 const MyBlog = ({ Component, pageProps }: AppPropsWithLayout) => {
     const getLayout = Component.getLayout ?? ((page) => page);
+
+    useEffect(() => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }, []);
+
     return getLayout(<Component {...pageProps} />);
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
     }
 }
 
+
 // const MyDocument = () => {
 //     const url = "<https://example.com>";
 //     const title = "Demo Next.js";
