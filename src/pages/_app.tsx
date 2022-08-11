31434d9a9602cba32ca9622b469e267de313fac8
riku0202 <riku0202excalibur@gmail.com>
2022-08-11 15:12:40 +0900

    fix: useLayoutEffectに変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode, useEffect } from "react";
+import { ReactElement, ReactNode, useLayoutEffect } from "react";
 import "../../public/reset.css";
 
 type NextPageWithLayout = NextPage & {
@@ -14,7 +14,7 @@ type AppPropsWithLayout = AppProps & {
 const MyBlog = ({ Component, pageProps }: AppPropsWithLayout) => {
     const getLayout = Component.getLayout ?? ((page) => page);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
     }, []);
